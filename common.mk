0debb9db6607f5cba80274a6860d14606304f87a
Andrew Thomas <44932193+itz63c@users.noreply.github.com>
2022-05-31 16:14:13 +0530

    sm8350-common: Add Notch Bar Killer overlay
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- common.mk
+++ common.mk
@@ -325,7 +325,8 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
-    WifiResTarget
+    WifiResTarget \
+    NotchBarKiller
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
