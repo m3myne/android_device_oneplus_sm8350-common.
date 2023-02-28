a4fc1b1a4bf08200540fd5ce07d5df4542c1df06
itsxrp <itsxrproms@gmail.com>
2023-02-28 07:32:36 +0100

    sm8350-common: Switch to clang-14
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,6 +107,10 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-14.0/bin
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-14.0
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 14.0
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
